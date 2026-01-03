0b808dda26a6424f9f84580551427baf37b4e729
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-03 22:26:22 +0000

    Update event_applier.py

--- a7do_core/event_applier.py
+++ a7do_core/event_applier.py
@@ -1,20 +1,2 @@
 def apply_event(a7do, event):
-    """
-    Applies a sensory experience to A7DO.
-    """
-
-    a7do.perceived_place = event.place
-
-    # Apply body intensity
-    a7do.body.apply_intensity(event.intensity)
-
-    # Familiarity observation
-    a7do.familiarity.observe(
-        place=event.place,
-        channels=event.channels,
-        intensity=event.intensity
-    )
-
-    a7do.log(
-        f"experienced pattern place={event.place} channels={list(event.channels.keys())}"
-    )
+    a7do.body.apply_intensity(event.get("intensity", 0.1))
