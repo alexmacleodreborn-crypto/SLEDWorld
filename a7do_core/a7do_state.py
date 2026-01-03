d8b2e22827287596f7504d976d9587709db84ccf
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-03 22:25:40 +0000

    Update a7do_state.py

--- a7do_core/a7do_state.py
+++ a7do_core/a7do_state.py
@@ -1,29 +1,25 @@
 from .body_state import BodyState
 from .familiarity import Familiarity
 
-
 class A7DOState:
+    """
+    The developing entity.
+    """
+
     def __init__(self):
         self.body = BodyState()
         self.familiarity = Familiarity(gated=True)
 
         self.birthed = False
-        self.is_awake = False
-        self.internal_log = []
+        self.aware = False
 
-    def tick(self, dt_seconds: float):
-        self.body.tick(dt_seconds)
+        self.internal_log = []
 
     def mark_birthed(self):
         self.birthed = True
-        self.familiarity.unlock()
         self.internal_log.append("birth: sensory shock")
 
-    def wake(self):
-        self.is_awake = True
-        self.internal_log.append("wake")
-
-    def sleep(self):
-        self.is_awake = False
-        replayed = self.familiarity.replay()
-        self.internal_log.append(f"sleep: replay {replayed}")
+    def unlock_awareness(self):
+        self.aware = True
+        self.familiarity.unlock()
+        self.internal_log.append("awareness unlocked")
