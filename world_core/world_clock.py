f88749d8a317dfccf5e23fa545e7930d6a445f41
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-03 20:32:25 +0000

    Update world_clock.py

--- world_core/world_clock.py
+++ world_core/world_clock.py
@@ -1,16 +1,44 @@
-def tick(self):
-    import time
+import time
 
-    if not self.started:
-        self.start()
-        return
 
-    now = time.time()
+class WorldClock:
+    def __init__(self):
+        self.started = False
+        self.last_time = None
+        self.seconds_elapsed = 0.0
 
-    if self.last_time is None:
+    def start(self):
+        now = time.time()
+        self.started = True
         self.last_time = now
-        return
 
-    delta = now - self.last_time
-    self.last_time = now
-    self.seconds_elapsed += delta
+    def tick(self):
+        """
+        Safe, self-healing tick.
+        Works even if start() was never called.
+        """
+        now = time.time()
+
+        if not self.started:
+            self.started = True
+            self.last_time = now
+            return
+
+        if self.last_time is None:
+            self.last_time = now
+            return
+
+        delta = now - self.last_time
+        self.last_time = now
+        self.seconds_elapsed += delta
+
+    @property
+    def days_elapsed(self):
+        # 1 simulated day = 24 real hours
+        return self.seconds_elapsed / 86400.0
+
+    def snapshot(self):
+        return {
+            "seconds_elapsed": round(self.seconds_elapsed, 2),
+            "days_elapsed": round(self.days_elapsed, 4),
+        }
