8af3b14508b4f766e03de9b2687b866182e1900f
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-03 21:43:16 +0000

    Create __init__.py

---
+++ world_core/__init__.py
@@ -0,0 +1,16 @@
+# sledworld/world_core/__init__.py
+
+"""
+World physics layer for SLED World.
+
+Contains:
+- time
+- biological sources (mother)
+- continuous fields (heartbeat, sound, pressure)
+"""
+
+__all__ = [
+    "heartbeat_field",
+    "mother_bot",
+    "world_clock",
+]
