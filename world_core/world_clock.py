6ef95eb508c20f7893a9f61829b6f0ddd6b7751b
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-03 20:31:06 +0000

    Update world_clock.py

--- world_core/world_clock.py
+++ world_core/world_clock.py
@@ -1,35 +1,16 @@
-# world_core/world_clock.py
+def tick(self):
+    import time
 
-class WorldClock:
-    """
-    Canonical world time.
-    Always running, independent of A7DO awareness.
-    """
+    if not self.started:
+        self.start()
+        return
 
-    def __init__(self):
-        self.running = False
-        self.minutes = 0.0  # world minutes since start
+    now = time.time()
 
-    def start(self):
-        self.running = True
+    if self.last_time is None:
+        self.last_time = now
+        return
 
-    def tick(self, delta_hours: float):
-        if not self.running:
-            return
-        self.minutes += delta_hours * 60.0
-
-    @property
-    def days_elapsed(self) -> float:
-        return self.minutes / (60.0 * 24.0)
-
-    def snapshot(self):
-        days = int(self.days_elapsed)
-        hours = int((self.minutes % (60 * 24)) // 60)
-        minutes = int(self.minutes % 60)
-
-        return {
-            "days": days,
-            "hours": hours,
-            "minutes": minutes,
-            "total_minutes": round(self.minutes, 2),
-        }
+    delta = now - self.last_time
+    self.last_time = now
+    self.seconds_elapsed += delta
