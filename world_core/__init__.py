792eb5bdf0207bb33cb44f5798d219b57665b663
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-04 18:37:04 +0000

    Update __init__.py

--- world_core/__init__.py
+++ world_core/__init__.py
@@ -2,7 +2,3 @@
 
 from .a7do_state import A7DOState
 from .day_cycle import DayCycle
-
-# IMPORTANT:
-# Do NOT import GestationBridge here.
-# It depends on world_core and must be imported explicitly where used.
