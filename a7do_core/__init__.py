8ba589e4df4fca744ebe874e4998283ad876ac0a
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-04 18:27:47 +0000

    Update __init__.py

--- a7do_core/__init__.py
+++ a7do_core/__init__.py
@@ -1,3 +1,9 @@
+# sledworld/a7do_core/__init__.py
+
 from .a7do_state import A7DOState
 from .day_cycle import DayCycle
-from .gestation_bridge import GestationBridge
+
+# NOTE:
+# Do NOT import GestationBridge here.
+# Import it directly where you use it:
+# from a7do_core.gestation_bridge import GestationBridge
