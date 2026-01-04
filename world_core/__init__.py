98229011ce54d833df3300f810ace6d328a23465
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-04 18:39:36 +0000

    Update __init__.py

--- world_core/__init__.py
+++ world_core/__init__.py
@@ -1,3 +1,18 @@
-# sledworld/a7do_core/__init__.py
+# world_core/__init__.py
+
+"""
+world_core
+
+Objective world simulation:
+- Time
+- Physics
+- Agents (MotherBot, later others)
+- No cognition
+- No subjective state
+"""
+
+from .world_clock import WorldClock
+from .mother_bot import MotherBot
+from .heartbeat_field import HeartbeatField
 
 
