af3fa86e69b8a888ef8deb5b178fc9061b741a04
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-09 18:32:01 +0000

    Update tv_profile.py

--- world_core/profiles/tv_profile.py
+++ world_core/profiles/tv_profile.py
@@ -1,4 +1,6 @@
 from world_core.sound.sound_source import SoundSource
+from world_core.light.light_source import LightSource
+
 
 class TVProfile:
     def __init__(self, name, position):
@@ -12,6 +14,13 @@ class TVProfile:
             base_level=0.6,
         )
 
+        self.light = LightSource(
+            name=f"{name}_light",
+            position=position,
+            base_level=1.0,
+        )
+
     def power_toggle(self):
         self.is_on = not self.is_on
         self.sound.set_active(self.is_on)
+        self.light.set_active(self.is_on)
