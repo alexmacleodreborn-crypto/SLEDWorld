aed87c386beb7396f33ccc46e630f7f65f550c42
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-10 19:38:12 +0000

    Update world_object.py

--- world_core/world_object.py
+++ world_core/world_object.py
@@ -1,23 +1,22 @@
 class WorldObject:
     def __init__(self, name: str, position):
         self.name = name
-        self.position = tuple(position)
+        self.position = tuple(float(x) for x in position)
         self.bounds = None
 
     def set_bounds(self, min_xyz, max_xyz):
         self.bounds = (tuple(min_xyz), tuple(max_xyz))
 
     def contains_world_point(self, xyz):
-        if not self.bounds:
+        if self.bounds is None:
             return False
-        (minx, miny, minz), (maxx, maxy, maxz) = self.bounds
+        (min_x, min_y, min_z), (max_x, max_y, max_z) = self.bounds
         x, y, z = xyz
-        return (minx <= x <= maxx) and (miny <= y <= maxy) and (minz <= z <= maxz)
+        return (min_x <= x <= max_x) and (min_y <= y <= max_y) and (min_z <= z <= max_z)
 
     def snapshot(self):
         return {
-            "type": "world_object",
             "name": self.name,
             "position": self.position,
-            "bounds": self.bounds,
+            "bounds": self.bounds
         }
